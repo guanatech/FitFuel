9560b37b6486c052b1a418ae904417658615c783
Egor <egeberleyn@gmail.com>
2023-06-13 18:21:00 +0300

    Add meal aggregate

---
+++ Fitfuel.Server/Modules/Meals/Fitfuel.Meals.Domain/MealAggregate/Enums/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace Fitfuel.Meals.Domain.MealAggregate.Enums;
+
+public enum Category
+{
+    None = 0,
+    Meat = 1,
+    Fish = 2,
+    Breakfast = 3,
+    Lunch = 4,
+    Water = 5,
+    Salad = 6,
+}
