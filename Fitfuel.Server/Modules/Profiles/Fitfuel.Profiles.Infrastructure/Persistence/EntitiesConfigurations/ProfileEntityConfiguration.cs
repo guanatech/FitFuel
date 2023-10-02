8177a2ac8a6c73c658de695aae0a1bc2200ac8a6
egor_geberleyn <egeberleyn@gmail.com>
2023-10-02 23:41:56 +0300

    Init infra profile module

---
+++ Fitfuel.Server/Modules/Profiles/Fitfuel.Profiles.Infrastructure/Persistence/EntitiesConfigurations/ProfileEntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Fitfuel.Profiles.Domain.ProfileAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Fitfuel.Profiles.Infrastructure.Persistence.EntitiesConfigurations;
+
+public class ProfileEntityConfiguration : IEntityTypeConfiguration<Profile>
+{
+    public void Configure(EntityTypeBuilder<Profile> builder)
+    {
+        builder.ToTable("profiles");
+
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.Id)
+            .ValueGeneratedNever()
+            .IsRequired();
+    }
+}
